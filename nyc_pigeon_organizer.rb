d36e1835c84ddc5ff276a5064d1425498ed08af5
Cole Rau <colevrau@gmail.com>
2019-10-30 20:40:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,8 @@
 def nyc_pigeon_organizer(pigeon_data)
-  
+  hash = {}
+  pigeon_data.each do |color_gender_lives, stats|
+    stats.each do |stats, names| 
+      names.each do |name|
+        if hash[name] == nil 
+          
 end
